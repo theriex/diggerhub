feb224a298f8b45b706c7bd1480f929a2df6a2c3
Eric Parker <theriex@gmail.com>
2021-01-17 20:54:59 -0500

    Initial deployment framework files

---
+++ site/public/js/app.js
@@ -0,0 +1,33 @@
+/*jslint browser, white, fudge */
+/*global window jtminjsDecorateWithUtilities */
+
+var app = {};  //Global container for application level funcs and values
+var jt = {};   //Global access to general utility methods
+
+(function () {
+    "use strict";
+
+    app.modules = [{name:"refmgr", desc:"Server data and client cache"}];
+
+
+    app.init2 = function () {
+        app.amdtimer.load.end = new Date();
+        jt.log("window.innerWidth: " + window.innerWidth);
+        jt.out("loadstatdiv", "");
+    };
+
+
+    app.init = function () {
+        var ox = window.location.href;
+        app.docroot = ox.split("/").slice(0, 3).join("/") + "/";
+        if(!jtminjsDecorateWithUtilities) { //support lib not loaded yet
+            return setTimeout(app.init, 50); }
+        jtminjsDecorateWithUtilities(jt);
+        var modules = app.modules.map((p) => "js/amd/" + p;);
+        jt.out("loadstatdiv", "Loading app modules...");
+        app.amdtimer = {};
+        app.amdtimer.load = { start: new Date() };
+        jt.loadAppModules(app, modules, app.baseurl, app.init2, "?v=210118");
+    };
+
+}());
