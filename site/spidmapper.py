4625125328f182ea7106a186d9133692a1d330ff
Eric Parker <theriex@gmail.com>
2021-04-23 18:18:29 -0400

    Mods after making it once through all current songs

--- site/spidmapper.py
+++ site/spidmapper.py
@@ -102,12 +102,39 @@ def fetch_spid(query):
 
 # Adjust known album name mismatches to what spotify wants.
 def fix_album_name(album):
-    sxps = [{"exp":r"Oumou\s+[\(\[]disk\s+1[\)\]]", "use":"Oumou"}]
+    sxps = [{"exp":r"Oumou\s+[\(\[]disk\s+1[\)\]]", "use":"Oumou"},
+            {"exp":r"Swiss\sMovement\s.*",
+             "use":"Swiss Movement (Montreux 30th Anniversary)"}]
     for sxp in sxps:
         abfix = re.sub(sxp["exp"], sxp["use"], album, flags=re.I)
         if abfix != album:
             return abfix
     return album
+
+
+# General compilations where the artist is "Various"
+def unpack_general_compilation(ti, ar, ab):
+    cxps = [{"abx":r"^God Save the Queen: 76-96.*", "spo":"arti", "sep":" / "},
+            {"abx":r"Racer Radio, Vol 1", "spo":"arti", "sep":" / "},
+            {"abx":r"Great 70's Dance Grooves", "spo":"arti", "sep":" / "}]
+    for cxp in cxps:
+        if re.match(cxp["abx"], ab, flags=re.I):
+            tes = ti.split(cxp["sep"])
+            if cxp["spo"] == "arti":
+                ar = tes[0]
+                ti = tes[1]
+            else:  # "tiar"
+                ti = tes[0]
+                ar = tes[1]
+            break
+    return ti, ar, ab
+
+
+# General alterations to be done before attempting a match
+def fix_known_bad_values(ti, ar, ab):
+    ab = fix_album_name(ab)
+    ti, ar, ab = unpack_general_compilation(ti, ar, ab)
+    return ti, ar, ab
 
 
 def remove_contextual_title_suffix(title, album):
@@ -117,21 +144,28 @@ def remove_contextual_title_suffix(title, album):
             {"abx":r"Paganini: 24 Caprices for Solo Violin, Op. 1",
              "tix":r"Op\.\s1/", "trt":"Op. 1, No. "},
             {"abx":r"Paganini: 24 Caprices for Solo Violin, Op. 1",
-             "tix":r" - .*", "trt":""}]
+             "tix":r" - .*", "trt":""},
+            {"abx":r"Storm The Studio", "tix":r"\(Part", "trt":"(Pt"}]
     for cxp in cxps:
         if re.match(cxp["abx"], album, flags=re.I):
             title = re.sub(cxp["tix"], cxp["trt"], title, flags=re.I)
     return title
 
 
+# safe to remove these if no match was found
 def remove_general_suffix(title):
     rxs = [r"\s*[\(\[]\d{4}\s+Digital\s+Remaster.*[\)\]]",
+           r"\s*[\(\[]\d{4}\s+Remastered\s+Version.*[\)\]]",
            r"\s*[\(\[]Remastered\s+Version.*[\)\]]",
-           r"\s*[\(\[]Explicit[\)\]]"]
+           r"\s*[\(\[]Remastered\s+LP\s+Version.*[\)\]]",
+           r"\s*[\(\[]Remixed\s+Album\s+Version.*[\)\]]",
+           r"\s*[\(\[]Explicit[\)\]]",
+           r"\s[\(\[]\d\d?\-\d\d?\-\d\d[\)\]]$",  # recording date
+           r"^\d\d?\s",  # title starts with track number
+           r"\.mp3$",
+           r"\s*[\(\[]Featuring\s+.*[\)\]]"]
     for rx in rxs:
-        tifix = re.sub(rx, "", title, flags=re.I)
-        if tifix != title:
-            return tifix
+        title = re.sub(rx, "", title, flags=re.I)
     return title
 
 
@@ -150,7 +184,25 @@ def reduce_collaborative_name(artist):
             {"exp":r"Patti\s+La\s+Belle", "use":"Patti LaBelle"},
             {"exp":r"Oceania", "use":"Jaz Coleman"},  # very ugly
             {"exp":r"^OMD$", "use":"Orchestral Manoeuvres In The Dark"},
-            {"exp":r"Midori", "use":"Niccolò Paganini"}]  # absurd
+            {"exp":r"Midori", "use":"Niccolò Paganini"},  # absurd
+            {"exp":r"Les McCann & Eddie Harris", "use":"Les McCann"},
+            {"exp":r"Curtis Mayfield & The Impressions",
+             "use":"Curtis Mayfield"},
+            {"exp":r"Amy X Newburg", "use":"Amy X Neuburg"},
+            {"exp":r"ClockDVA", "use":"Clock DVA"},
+            {"exp":r"Miles Davis Sextet", "use":"Miles Davis"},
+            {"exp":r"Time Zone featuring .*", "use":"Time Zone"},
+            {"exp":r"Mickey Hart, Zakir Hussain, Sikiru Adepoju, .*",
+             "use":"Sikiru Adepoju"},
+            {"exp":r"Jello Biafra With Nomeansno", "use":"Jello Biafra"},
+            {"exp":r"Ian Dury & The Blockheads", "use":"Ian Dury"},
+            {"exp":r"Stevie Ray Vaughan & Double Trouble",
+             "use":"Stevie Ray Vaughan"},
+            {"exp":r"Hassan Hakmoun And Zahar", "use":"Hassan Hakmoun"},
+            {"exp":r"Robert Fripp and Brian Eno", "use":"Robert Fripp"},
+            {"exp":r"Band of Gypsys", "use":"Jimi Hendrix"},
+            {"exp":r"Gary Clail and On-U Sound System", "use":"Gary Clail"},
+            {"exp":r"Gary Clail & On-U Sound System", "use":"Gary Clail"}]
     for sxp in sxps:
         artfix = re.sub(sxp["exp"], sxp["use"], artist, flags=re.I)
         if artfix != artist:
@@ -161,7 +213,7 @@ def reduce_collaborative_name(artist):
 # e.g. find_spid("I'm Every Woman", "Chaka Khan", "Epiphany - The Best Of Chaka Khan Vol 1")
 def find_spid(ti, ar, ab):
     verify_token()
-    ab = fix_album_name(ab)
+    ti, ar, ab = fix_known_bad_values(ti, ar, ab)
     skm = {"qtxt":make_query_string(ti, ar, ab), "qtype":"tiarab"}
     spid = fetch_spid(skm["qtxt"])
     # ti: "whatever song (2015 Digital Remaster) is not how Spotify lists it.
