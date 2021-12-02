9a4eae6301170da001efab9983af7721e005c4dd
Eric Parker <theriex@gmail.com>
2021-12-02 10:46:02 -0500

    released v0.9.12

--- site/py/start.py
+++ site/py/start.py
@@ -6,7 +6,7 @@
 import logging
 import py.util as util
 
-CACHE_BUST_PARAM = "v=211129"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=211202"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
