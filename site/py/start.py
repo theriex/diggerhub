34b22ace192ea1b644a8728c93759a4bef59336e
Eric Parker <theriex@gmail.com>
2022-08-19 17:18:26 -1000

    release version 1.0.6 versionCode 5

--- site/py/start.py
+++ site/py/start.py
@@ -6,7 +6,7 @@
 import logging
 import py.util as util
 
-CACHE_BUST_PARAM = "v=220619"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=220819"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
--- site/py/util.py
+++ site/py/util.py
@@ -23,7 +23,7 @@ import py.dbacc as dbacc
 import py.mconf as mconf
 
 def version():
-    return "v1.0.5"
+    return "v1.0.6"
 
 
 def srverr(msg, code=400):
