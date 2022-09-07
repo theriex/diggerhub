0819bb2372848eb527e992f9987bc3d248945d95
Eric Parker <theriex@gmail.com>
2022-09-07 12:31:03 +1000

    release version v1.0.8 versionCode 8

--- site/py/start.py
+++ site/py/start.py
@@ -6,7 +6,7 @@
 import logging
 import py.util as util
 
-CACHE_BUST_PARAM = "v=220822"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=220907"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
