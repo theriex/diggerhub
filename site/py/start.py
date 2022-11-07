d250dcdb222c3beae02629b7426fce4de0631a1c
Eric Parker <theriex@gmail.com>
2022-11-07 11:41:13 -1000

    release version v1.0.10 versionCode 11

--- site/py/start.py
+++ site/py/start.py
@@ -6,7 +6,7 @@
 import logging
 import py.util as util
 
-CACHE_BUST_PARAM = "v=221003"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=221107"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
