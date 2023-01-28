35ca3e47213d2dbedf100853783c290af93facb3
Eric Parker <theriex@gmail.com>
2023-01-27 16:12:01 -1000

    Site general info, account access and download improvements

--- site/py/start.py
+++ site/py/start.py
@@ -6,7 +6,7 @@
 import logging
 import py.util as util
 
-CACHE_BUST_PARAM = "v=221206"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=230128"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
