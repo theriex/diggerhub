f8e3998c1375b5fb6c79591d6784b39a1f81881e
Eric Parker <theriex@gmail.com>
2023-03-08 13:28:29 -1000

    Minor verbiage improvement

--- site/py/start.py
+++ site/py/start.py
@@ -6,7 +6,7 @@
 import logging
 import py.util as util
 
-CACHE_BUST_PARAM = "v=230306"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=230308"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
