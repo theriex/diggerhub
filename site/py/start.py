bb5e8b5b40c5b3dee15db91901935ed7f0b2e50f
Eric Parker <theriex@gmail.com>
2025-02-14 08:28:12 -0500

    Fixed sign in form process sequencing for beta page

--- site/py/start.py
+++ site/py/start.py
@@ -12,7 +12,7 @@ import py.dbacc as dbacc
 import io
 from PIL import Image, ImageDraw, ImageFont
 
-CACHE_BUST_PARAM = "v=250211"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=250214"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
