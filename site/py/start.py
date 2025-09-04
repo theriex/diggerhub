99d824488ea4343c3b6e16387993fbacf9f74a61
Eric Parker <theriex@gmail.com>
2025-09-04 09:54:34 -0400

    release version 1.6.42, versionCode 69, Build 52

--- site/py/start.py
+++ site/py/start.py
@@ -15,7 +15,7 @@ from PIL import Image, ImageDraw, ImageFont
 import json
 import datetime
 
-CACHE_BUST_PARAM = "v=250902"  # Updated via ../../build/cachev.js
+CACHE_BUST_PARAM = "v=250904"  # Updated via ../../build/cachev.js
 
 INDEXHTML = """
 <!doctype html>
